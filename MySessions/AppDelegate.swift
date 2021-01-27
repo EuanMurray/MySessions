31ca6ee7f67d0a69acb1faf5f6303c48a4005b19
Euan <euanm2514@gmail.com>
2021-01-27 12:17:06 -0500

    Initial Commit for MySessions

---
+++ MySessions/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MySessions
+//
+//  Created by Euan Murray on 2021-01-26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
